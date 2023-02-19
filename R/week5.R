840e1cf9d0948e51ee99936c12e3ae1070d55848
agreco <greco031@umn.edu>
2023-02-18 21:22:16 -0600

    Second pipe complete!

--- R/week5.R
+++ R/week5.R
@@ -7,7 +7,7 @@ library("lubridate")
 Adata_tbl <- read_delim(file ="../data/Aparticipants.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 Anotes_tbl <- read_csv(file ="../data/Anotes.csv")
 Bdata_tbl <- read_delim(file ="../data/Bparticipants.dat", delim = "\t", col_names = c("casenum", "parnum", "stimver", "datadate", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"))
-Bnotes_tbl <- read_delim(file ="../data/Bnotes.txt", delim = "\t", col_names = c("casenum", "notes"))
+Bnotes_tbl <- read_delim(file ="../data/Bnotes.txt", delim = "\t")
 
 # Data Cleaning
 Aclean_tbl <- Adata_tbl %>%
@@ -16,3 +16,10 @@ Aclean_tbl <- Adata_tbl %>%
   mutate(across(q1:q5, ~as.integer(.))) %>%
   right_join(Anotes_tbl, by = "parnum") %>%
   filter(is.na(notes))
+Bclean_tbl <- Bdata_tbl %>%
+  mutate(datadate = mdy_hms(datadate)) %>%
+  mutate(across(q1:q10, ~as.integer(.))) %>%
+  right_join(Bnotes_tbl, by = "parnum") %>%
+  filter(is.na(notes)) %>%
+  bind_rows(Aclean_tbl, .id = "lab") %>%
+  select(-notes)
